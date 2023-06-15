057ef6ceaf728f49ef1eb9162a7fb86ba556c068
reddeath1 <frankslayer1@gmail.com>
2023-06-15 20:44:54 +0300

    avarage weight

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def weight_average(my_list=[]):
+	"""
+	Write a function that returns the weighted average of all integers tuple (<score>, <weight>)
+	"""
+	if len(my_list) == 0:
+		return 0
+	ss = [score*weight for (score, weight) in my_list]
+	return sum(ss) / sum([weight for (score, weight) in my_list])
