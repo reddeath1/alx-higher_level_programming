8a14924bdcea01127ef3d8eedf4d212608489475
reddeath1 <frankslayer1@gmail.com>
2023-06-15 20:49:37 +0300

    avarage weight

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -6,5 +6,5 @@ def weight_average(my_list=[]):
 	"""
 	if len(my_list) == 0:
 		return 0
-	ss = [score*weight for (score, weight) in my_list]
-	return sum(ss) / sum([weight for (score, weight) in my_list])
+	scores = [score*weight for (score, weight) in my_list]
+	return sum(scores) / sum([weight for (score, weight) in my_list])
