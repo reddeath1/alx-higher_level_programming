27a547deb23d591496178bb64d45903cbc51c4c8
reddeath1 <frankslayer1@gmail.com>
2023-07-11 01:03:53 +0300

    adding task 2

---
+++ 0x0A-python-inheritance/tests/2-is_same_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+@author: Frank Galos
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Check if two objects are the same class
+    """
+    if not isinstance(a_class, type):
+        raise TypeError("a_class must be a type")
+    return (type(obj) is a_class)
