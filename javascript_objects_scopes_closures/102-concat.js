8dea22dabe8236165638bbc6a3bae38d5bc39d91
Reddeath <frankslayer1@gmail.com>
2023-09-13 01:17:27 +0300

    0x13-javascript_objects_scopes_closures

--- javascript_objects_scopes_closures/102-concat.js
+++ javascript_objects_scopes_closures/102-concat.js
@@ -2,8 +2,8 @@
 const argv = process.argv;
 const file = require('fs');
 const fileA = argv[2];
-const textA = file.readFileSync(fileA, 'utf8');
+const tA = file.readFileSync(fileA, 'utf8');
 const fileB = argv[3];
-const textB = file.readFileSync(fileB, 'utf8');
+const tB = file.readFileSync(fileB, 'utf8');
 const fileC = argv[4];
-file.writeFileSync(fileC, textA + textB);
+file.writeFileSync(fileC, tA + tB);
