684f3fa51bf697f21c8b9442d4458dd64d7f9fbd
Reddeath <frankslayer1@gmail.com>
2023-09-17 02:07:36 +0300

    model city

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+script for using sqlalchemy to model the models using ORM
+"""
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base
+
+class City(Base):
+    """
+    cities class for use with sqlalchemy inherits from sqlalchemy
+    declarative_base
+    """
+    __tablename__ = 'cities'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("state.id"), nullable=False)
