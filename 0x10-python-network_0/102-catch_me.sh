001e320bf76bf2e30e19a66199f7c7b9cae153da
Reddeath <frankslayer1@gmail.com>
2023-09-29 01:33:56 +0300

    python network

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Bash script to make a request that triggers the server's response
-
-curl -s -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" "0.0.0.0:5000/catch_me"
+curl -sLX PUT -d "user_id:98" -H "Origin: School" 0.0.0.0:5000/catch_me
