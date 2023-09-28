9ccaf984e3b2ae52e5f6590d2d3bbb562b96306e
Reddeath <frankslayer1@gmail.com>
2023-09-29 01:27:17 +0300

    python network

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 # Bash script to send a request and display the status code
 
-# Check if an argument (URL) is provided
-if [ $# -ne 1 ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
-# Use curl to send the request and display the status code
 curl -s -I -o /dev/null -w "%{http_code}" "$1"
