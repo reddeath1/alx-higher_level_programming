7c6399f978a585b844fa98650d8156d852a43bde
Reddeath <frankslayer1@gmail.com>
2023-09-29 01:23:53 +0300

    python network

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-# send a request to a URL passed as an argument, 
-#and displays only the status code of the response.
-curl -s -o /dev/null -w "%{http_code}" "$1"
+# Bash script to extract the HTTP status code from a previous curl request
+
+# Check if an argument (the previous response) is provided
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <previous_response>"
+  exit 1
+fi
+
+# Extract the HTTP status code using awk
+http_code=$(echo "$1" | awk '{print $NF}')
+
+# Display the status code
+echo "Status code: $http_code"
